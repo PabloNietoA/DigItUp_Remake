5ba8338849faea3bc79b1f71f3518a0b7b62347f
Cris/ <cristimg9@gmail.com>
2024-08-09 15:50:02 +0200

    Funcion no usada borrada

--- Assets/Scripts/Minable.cs
+++ Assets/Scripts/Minable.cs
@@ -7,15 +7,6 @@ public class Minable : MonoBehaviour
     [SerializeField] int value;
     private bool notCollided = true;
 
-    void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.tag == "Player"){
-            if(notCollided) Manager.instance.addMoney(value);
-            notCollided = false;
-            Destroy(gameObject);
-        }
-
-    }
-
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.gameObject.tag == "Player"){
             if(notCollided) Manager.instance.addMoney(value);
